4650552f3ef7d21a557d79a5cc8ac1b5bda0a2d1
rudylucky <1284582372@qq.com>
2019-12-15 15:33:36 +0000

    update config remove alias for absolute import

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import BaseLayout from 'src/components/layout/BaseLayout';
+import BaseLayout from 'components/layout/BaseLayout';
 
 const App: React.FC = () => {
   return (
