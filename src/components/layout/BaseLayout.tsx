6f35b218e08155e07f593a397a279bd6a1ee1381
rudylucky <1284582372@qq.com>
2019-12-15 15:26:54 +0000

    update config

---
+++ src/components/layout/BaseLayout.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BaseLayout: React.FC = () => {
+  return (
+    <div className="base-layout">
+      base layout works!
+    </div>
+  );
+}
+
+export default BaseLayout;
