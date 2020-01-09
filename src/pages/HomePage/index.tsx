2625a990ec95574c5093dceda412e7c8eac456bf
rudylucky <rudylucky@163.com>
2020-01-09 15:02:19 +0000

    update

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -11,13 +11,21 @@ const HomePage = (props: AppHomePage) => {
   return (
     <div className={style.homePage}>
       <div className={style.imgContainer}>
-        <div className={style.slide}>1
-          {/* <img src="https://i.loli.net/2019/12/26/MxjikTyo3QlDUdp.jpg" width="100%"></img> */}
+        <div className={style.slide}>
+          <img src="https://i.loli.net/2019/12/26/MxjikTyo3QlDUdp.jpg" width="100%"></img>
         </div>
-        <div className={style.slide}>2</div>
-        <div className={style.slide}>3</div>
-        <div className={style.slide}>4</div>
-        <div className={style.slide}>5</div>
+        <div className={style.slide}>
+          <img src="https://i.loli.net/2019/12/26/MxjikTyo3QlDUdp.jpg" width="100%"></img>
+        </div>
+        <div className={style.slide}>
+          <img src="https://i.loli.net/2019/12/26/MxjikTyo3QlDUdp.jpg" width="100%"></img>
+        </div>
+        <div className={style.slide}>
+          <img src="https://i.loli.net/2019/12/26/MxjikTyo3QlDUdp.jpg" width="100%"></img>
+        </div>
+        <div className={style.slide}>
+          <img src="https://i.loli.net/2019/12/26/MxjikTyo3QlDUdp.jpg" width="100%"></img>
+        </div>
       </div>
     </div>
   )
