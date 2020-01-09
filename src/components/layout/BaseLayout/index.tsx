621cd1900abe7af19dd57888c46caf1a8acb5a9f
liudi <rudylucky@163.com>
2020-01-09 10:12:20 +0000

    update route

--- src/components/layout/BaseLayout/index.tsx
+++ src/components/layout/BaseLayout/index.tsx
@@ -2,15 +2,24 @@ import React, { useState } from 'react'
 import AppHeader from '../AppHeader'
 import AppMenu from '../AppMenu'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import route from 'common/route'
+import route, { AppRoute } from 'common/route'
 import style from './index.module.scss'
 import { ArticleEntity } from 'services/ArticleService'
 import HomePage from 'pages/HomePage'
 
 
+function routers(config: Array<AppRoute>, prefix: string = '') {
+  config.map(v => {
+    if (!v.children) {
+      return <Route exact path={v.path} key={v.code} component={v.component}></Route>
+    }
+  })
+}
+
 const BaseLayout = () => {
 
   const routers = route.map(v => <Route exact path={v.path} key={v.code} component={v.component}></Route>)
+
 
   return (
     <div className={style.baseLayout}>
